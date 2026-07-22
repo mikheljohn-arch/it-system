bd369c8ea6c7926381fd24882a509f8f6795533c
Mikhel John <mikheljohn@gmail.com>
2026-07-23 07:53:50 +0800

    fix: exclude API routes from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -43,5 +43,6 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|api).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|api|sw.js).*)'],
 }
+
